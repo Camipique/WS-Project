9afdc97b486bcb190834c6ca4c680ef48e212a90
Camipique <c.quendera@campus.fct.unl.pt>
2020-06-11 00:46:57 +0100

    Reduced dataset sizes to 2000.

--- utils/process_avazu.py
+++ utils/process_avazu.py
@@ -9,8 +9,8 @@ import pandas as pd
 # Avazu dataset to 4000 samples
 
 n_rows = 4577464
-skip = np.delete(np.arange(n_rows), np.arange(0, n_rows, 1144))
+skip = np.delete(np.arange(n_rows), np.arange(0, n_rows, 2290))
 
-csv = pd.read_csv("../data/avazu/test.csv", skiprows = skip)[:-4]
+csv = pd.read_csv("../data/avazu/test.csv", skiprows = skip)
 csv.to_csv("../data/avazu/avazu_sample.csv")
 
