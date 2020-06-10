0cc272df9f9f0a0a297070a90130667ab411ac78
Camipique <c.quendera@campus.fct.unl.pt>
2020-06-10 22:57:44 +0100

    Added methods to compute AUC, LogLoss and RMSE metrics.

--- metrics.py
+++ metrics.py
@@ -1,5 +1,23 @@
 # -*- coding: utf-8 -*-
 """
 @author: Carlos
+
+
+Metrics: 
+    - AUC
+    - Log Loss
+    - RMSE
 """
 
+from sklearn.metrics import log_loss, roc_auc_score, mean_squared_error
+
+from math import sqrt
+
+def compute_auc(true_y, pred_y):
+    return round(roc_auc_score(true_y, pred_y), 4)
+
+def compute_log_loss(true_y, pred_y):
+    return round(log_loss(true_y, pred_y), 4)
+
+def compute_rmse(true_y, pred_y):
+    return round(sqrt(mean_squared_error(true_y, pred_y)), 4)
