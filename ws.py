71a275578394338bccb8873bff685e1a962e1596
Camipique <c.quendera@campus.fct.unl.pt>
2020-06-11 01:52:54 +0100

    Dataset splitting in train and test subsets. Saving the subsets in the dataset correspondent folder

--- ws.py
+++ ws.py
@@ -8,6 +8,7 @@ import time
 import pandas as pd
 
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler
+from sklearn.model_selection import train_test_split
 
 def process_criteo():
     print("Reading criteo data...")
@@ -33,6 +34,9 @@ def process_criteo():
     
     print("Saving processed criteo data...\n")
     data_criteo.to_csv("./data/criteo/criteo_sample_processed.csv")
+    
+    print("Creating train and test subsets on criteo data...\n")
+    split_dataset(data_criteo, "criteo", 0.5)
     
     return data_criteo
     
@@ -61,16 +65,27 @@ def process_avazu():
     print("Saving processed avazu data...\n")
     data_avazu.to_csv("./data/avazu/avazu_sample_processed.csv")
     
-    return data_avazu
+    print("Creating train and test subsets on avazu data...\n")
+    split_dataset(data_avazu, "avazu", 0.5)
     
+    return data_avazu
 
+"""
+    dataset: dataset to split into train and test subsets
+    dataset_name: dataset name
+    test_proportion: proportion of the test subset. default is 20% of the size of the set
+"""
+def split_dataset(dataset, dataset_name, test_proportion = 0.2):
+    train, test = train_test_split(dataset, test_size=test_proportion, stratify = dataset["label"])
+    
+    train.to_csv("./data/{name}/train_{size}_{name}.csv".format(name = dataset_name, size = 1 - test_proportion))
+    test.to_csv("./data/{name}/test_{size}_{name}.csv".format(name = dataset_name, size = test_proportion))
+    
+    
 if __name__ == "__main__":
-
     start_time = time.time()
     
     data_criteo = process_criteo()
     data_avazu = process_avazu()
-    
-    
-    
+        
     print("Program ended in {time} seconds.".format(time = round(time.time() - start_time, 2)))
